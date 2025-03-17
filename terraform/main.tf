0a957be8cf00e4051aaf2eb815f59af79fe667e7
Your Name <you@example.com>
2025-03-17 10:45:21 +0000

    dockerize

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,7 +81,7 @@ resource "aws_instance" "oleg_builder" {
     amazon-linux-extras install docker -y
     systemctl start docker
     systemctl enable docker
-    usermod -a -G docker ec2-user
+    usermod -a -G docker ubuntu
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
   EOF
